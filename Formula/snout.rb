cfbb574ef0192b7e0859a0875b8b89b206f110df
czyt <czytcn@gmail.com>
2026-04-16 18:48:07 +0800

    feat: add Snout Homebrew Formula with auto-update workflow
    Snout is a Rime input method initialization and update tool supporting:
    - Wanxiang (万象)
    - Wusong (雾凇)
    - Baishuang (白霜)
    - Bohe (薄荷) schemes
    
    Initial version 0.1.1 with placeholder sha256s (workflow will update).

---
+++ Formula/snout.rb
@@ -0,0 +1,42 @@
+class Snout < Formula
+  desc "Rime input method initialization and update tool - supports Wanxiang/Wusong/Baishuang/Bohe schemes"
+  homepage "https://github.com/ca-x/snout"
+  version "0.1.1"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/ca-x/snout/releases/download/v0.1.1/snout-v0.1.1-macos-aarch64"
+      sha256 "PLACEHOLDER"
+
+      def install
+        bin.install "snout-v0.1.1-macos-aarch64" => "snout"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ca-x/snout/releases/download/v0.1.1/snout-v0.1.1-macos-x86_64"
+      sha256 "PLACEHOLDER"
+
+      def install
+        bin.install "snout-v0.1.1-macos-x86_64" => "snout"
+      end
+    end
+  end
+
+  def caveats
+    <<~EOS
+      snout has been installed!
+      Run 'snout --help' to get started.
+
+      Supported Rime schemes:
+      - Wanxiang (万象)
+      - Wusong (雾凇)
+      - Baishuang (白霜)
+      - Bohe (薄荷)
+    EOS
+  end
+
+  test do
+    system "#{bin}/snout", "--version"
+  end
+end
